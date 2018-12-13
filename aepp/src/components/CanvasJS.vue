41e03661e9b5b83095db0e9e628c7227dc9923ec
keno dressel <aeternity@keno.digital>
2018-12-13 13:12:07 +0100

    moves handler functions from mounted hook to methods

--- aepp/src/components/CanvasJS.vue
+++ aepp/src/components/CanvasJS.vue
@@ -17,7 +17,12 @@
         layer: null,
         overlayLayer: null,
         windowImages: [],
-        overlayImageId: null
+        overlayImageId: null,
+        lastDist: 0,
+        lastPos: {
+          x: -1,
+          y: -1
+        }
       }
     },
     computed: {
@@ -99,7 +104,177 @@
           width: this.stage.width(),
           height: this.stage.height()
         }
-      }
+      },
+
+      // DRAG, ZOOM AND NAVIGATIONAL STUFF
+
+      getDistance (p1, p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2))
+      },
+      onTouchMoveEvent (evt) {
+        let stage = this.stage;
+        let touch1 = evt.touches[0]
+        let touch2 = evt.touches[1]
+
+        if (touch1 && touch2) {
+          let dist = this.getDistance({
+            x: touch1.clientX,
+            y: touch1.clientY
+          }, {
+            x: touch2.clientX,
+            y: touch2.clientY
+          })
+
+          if (!this.lastDist) {
+            this.lastDist = dist
+          }
+          let oldScale = stage.getScaleX()
+
+          let offsetTop = this.$refs.stageWrapper.offsetTop
+          let offsetLeft = this.$refs.stageWrapper.offsetLeft
+
+          let center = {
+            x: (touch1.clientX + touch2.clientX) / 2 - offsetLeft,
+            y: (touch1.clientY + touch2.clientY) / 2 - offsetTop
+          }
+
+          let mousePointTo = {
+            x: center.x / oldScale - stage.x() / oldScale,
+            y: center.y / oldScale - stage.y() / oldScale,
+          }
+
+          let scale = stage.getScaleX() * dist / this.lastDist
+
+          stage.scaleX(scale)
+          stage.scaleY(scale)
+
+          let newPos = {
+            x: -(mousePointTo.x - center.x / scale) * scale,
+            y: -(mousePointTo.y - center.y / scale) * scale
+          }
+
+          stage.position(newPos)
+
+          stage.draw()
+          this.lastDist = dist
+
+        } else if (touch1) {
+          let offsetTop = this.$refs.stageWrapper.offsetTop
+          let offsetLeft = this.$refs.stageWrapper.offsetLeft
+
+          // GET OLD POS
+          let { x, y } = this.lastPos
+
+          // CHECK IF OLD POS IS NOT DEFAULT
+          if (x !== -1 && y !== -1) {
+            // CHECK IF FINGER MOVED
+            let dist = this.getDistance({
+              x: touch1.clientX - offsetLeft,
+              y: touch1.clientY - offsetTop
+            }, this.lastPos)
+
+            if (dist > 0) {
+
+              // MOVE CANVAS
+              let diff = {
+                xDiff: touch1.clientX - offsetLeft - this.lastPos.x,
+                yDiff: touch1.clientY - offsetTop - this.lastPos.y,
+              }
+
+              this.moveCanvas(diff)
+
+              this.lastPos = {
+                x: touch1.clientX - offsetLeft,
+                y: touch1.clientY - offsetTop
+              }
+            }
+          }
+        }
+      },
+      onTouchEndEvent(event) {
+        this.lastDist = 0;
+        this.lastPos = {
+          x: -1,
+          y: -1
+        }
+      },
+      onTouchStartEvent(event) {
+        let touch1 = event.touches[0]
+
+        if (event.touches.length === 1) {
+          this.lastPos = {
+            x: touch1.clientX - this.$refs.stageWrapper.offsetLeft,
+            y: touch1.clientY - this.$refs.stageWrapper.offsetTop
+          }
+        }
+      },
+      onWheelEvent(event) {
+        let scaleBy = 1.2
+        event.preventDefault()
+        let oldScale = this.stage.scaleX()
+
+        let mousePointTo = {
+          x: this.stage.getPointerPosition().x / oldScale - this.stage.x() / oldScale,
+          y: this.stage.getPointerPosition().y / oldScale - this.stage.y() / oldScale,
+        }
+
+        let newScale = event.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy
+        console.log(newScale)
+        //this.stage.scale({ x: newScale, y: newScale })
+        this.stage.scaleX(newScale)
+        this.stage.scaleY(newScale)
+
+        let newPos = {
+          x: -(mousePointTo.x - this.stage.getPointerPosition().x / newScale) * newScale,
+          y: -(mousePointTo.y - this.stage.getPointerPosition().y / newScale) * newScale
+        }
+        this.stage.position(newPos)
+        this.stage.draw()
+      },
+      onMouseDownEvent(event) {
+        this.lastPos = {
+          x: event.clientX - this.$refs.stageWrapper.offsetLeft,
+          y: event.clientY - this.$refs.stageWrapper.offsetTop
+        }
+      },
+      onMouseUpEvent(event) {
+        this.lastPos = {
+          x: -1,
+          y: -1
+        }
+      },
+      onMouseMoveEvent(event) {
+        let offsetTop = this.$refs.stageWrapper.offsetTop
+        let offsetLeft = this.$refs.stageWrapper.offsetLeft
+
+        // GET OLD POS
+        let { x, y } = this.lastPos
+
+        // CHECK IF OLD POS IS NOT DEFAULT
+        if (x !== -1 && y !== -1) {
+          // CHECK IF FINGER MOVED
+          let dist = this.getDistance({
+            x: event.clientX - offsetLeft,
+            y: event.clientY - offsetTop
+          }, this.lastPos)
+
+          if (dist > 0) {
+
+            // MOVE CANVAS
+            let diff = {
+              xDiff: event.clientX - offsetLeft - this.lastPos.x,
+              yDiff: event.clientY - offsetTop - this.lastPos.y,
+            }
+
+            this.moveCanvas(diff)
+
+            this.lastPos = {
+              x: event.clientX - offsetLeft,
+              y: event.clientY - offsetTop
+            }
+          }
+        }
+      },
     },
     created () {
       this.$store.dispatch(`loadImages`)
@@ -123,119 +298,37 @@
       })
 
       if (this.draggable) {
-        let stage = this.stage
-        let lastDist = 0
-        let lastPos = {
-          x: -1,
-          y: -1
-        }
+        /*
+        MOBILE DRAG AND ZOOM HANDLER
+         */
 
-        function getDistance (p1, p2) {
-          return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2))
-        }
+        this.stage.getContent().addEventListener('touchstart', this.onTouchStartEvent, false);
+        this.stage.getContent().addEventListener('touchmove', this.onTouchMoveEvent, false)
+        this.stage.getContent().addEventListener('touchend', this.onTouchEndEvent, false)
 
-        stage.getContent().addEventListener('touchstart', (evt) => {
-          let touch1 = evt.touches[0]
+        /*
+        DESKTOP DRAG AND ZOOM HANDLER
+         */
 
-          if(evt.touches.length === 1) {
-            lastPos = {
-              x: touch1.clientX - this.$refs.stageWrapper.offsetLeft,
-              y: touch1.clientY - this.$refs.stageWrapper.offsetTop
-            }
-          }
-
-        })
-
-        stage.getContent().addEventListener('touchmove', (evt) => {
-          let touch1 = evt.touches[0]
-          let touch2 = evt.touches[1]
-
-          if (touch1 && touch2) {
-            let dist = getDistance({
-              x: touch1.clientX,
-              y: touch1.clientY
-            }, {
-              x: touch2.clientX,
-              y: touch2.clientY
-            })
-
-            if (!lastDist) {
-              lastDist = dist
-            }
-            let oldScale = stage.getScaleX()
-
-            let offsetTop = this.$refs.stageWrapper.offsetTop
-            let offsetLeft = this.$refs.stageWrapper.offsetLeft
-
-            let center = {
-              x: (touch1.clientX + touch2.clientX) / 2 - offsetLeft,
-              y: (touch1.clientY + touch2.clientY) / 2 - offsetTop
-            }
-
-            let mousePointTo = {
-              x: center.x / oldScale - stage.x() / oldScale,
-              y: center.y / oldScale - stage.y() / oldScale,
-            }
-
-            let scale = stage.getScaleX() * dist / lastDist
-
-            stage.scaleX(scale)
-            stage.scaleY(scale)
-
-            let newPos = {
-              x: -(mousePointTo.x - center.x / scale) * scale,
-              y: -(mousePointTo.y - center.y / scale) * scale
-            }
-
-            stage.position(newPos)
-
-            stage.draw()
-            lastDist = dist
-          } else if (touch1) {
-            let offsetTop = this.$refs.stageWrapper.offsetTop
-            let offsetLeft = this.$refs.stageWrapper.offsetLeft
-
-            // GET OLD POS
-            let { x, y } = lastPos
-
-            // CHECK IF OLD POS IS NOT DEFAULT
-            if (x !== -1 && y !== -1) {
-              // CHECK IF FINGER MOVED
-              let dist = getDistance({
-                x: touch1.clientX - offsetLeft,
-                y: touch1.clientY - offsetTop
-              }, lastPos)
-
-              if (dist > 0) {
-
-                // MOVE CANVAS
-                let diff = {
-                  xDiff: touch1.clientX - offsetLeft - lastPos.x,
-                  yDiff: touch1.clientY - offsetTop - lastPos.y,
-                }
-
-                this.moveCanvas(diff)
-
-                lastPos = {
-                  x: touch1.clientX - offsetLeft,
-                  y: touch1.clientY - offsetTop
-                }
-              }
-            }
-          }
-        }, false)
-
-        stage.getContent().addEventListener('touchend', function () {
-          lastDist = 0
-          lastPos = {
-            x: -1,
-            y: -1
-          }
-        }, false)
+        this.stage.getContent().addEventListener('wheel', this.onWheelEvent);
+        this.stage.getContent().addEventListener('mousedown', this.onMouseDownEvent);
+        this.stage.getContent().addEventListener('mousemove', this.onMouseMoveEvent);
+        this.stage.getContent().addEventListener('mouseup', this.onMouseUpEvent);
+        this.stage.getContent().addEventListener('mouseleave', this.onMouseUpEvent);
       }
 
       console.log('canvas mounted')
-    }
+    },
+    beforeDestroy: function () {
+      this.stage.getContent().removeEventListener('touchstart', this.onTouchStartEvent, false);
+      this.stage.getContent().removeEventListener('touchmove', this.onTouchMoveEvent, false)
+      this.stage.getContent().removeEventListener('touchend', this.onTouchEndEvent, false)
+      this.stage.getContent().removeEventListener('wheel', this.onWheelEvent)
+      this.stage.getContent().removeEventListener('mousedown', this.onMouseDownEvent);
+      this.stage.getContent().removeEventListener('mousemove', this.onMouseMoveEvent);
+      this.stage.getContent().removeEventListener('mouseup', this.onMouseUpEvent);
+      this.stage.getContent().removeEventListener('mouseleave', this.onMouseUpEvent);
+    },
   }
 </script>
 
