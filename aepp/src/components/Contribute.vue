1264e1966d74e5f162e37a91cba7b5bdc6af5b93
keno dressel <aeternity@keno.digital>
2018-12-13 13:20:27 +0100

    introduces some basic error handling

--- aepp/src/components/Contribute.vue
+++ aepp/src/components/Contribute.vue
@@ -9,18 +9,26 @@
           <input type="file" name="image" ref="uploadInput"
                  @change="filesChange($event.target.files)"
                  accept="image/*" class="absolute pin opacity-0 w-full h-full">
-          <p v-if="isInitial" class="p-4" >
+          <p v-if="isInitial" class="p-4">
             Drag your file(s) here to begin<br> or click to browse
           </p>
         </div>
       </form>
       <div v-if="isSuccess">
-        <div class="w-full flex justify-center">
+        <div class="w-full flex justify-center p-4">
           <img ref="image" class="w-full h-full" :src="uploadedImage.src" :alt="uploadedImage.name">
         </div>
         <div class="w-full flex justify-center p-4">
           <ae-button class="mr-4" type="normal" @click="reset">Reset</ae-button>
           <ae-button type="dramatic" @click="$router.push(`render`)">Preview Artwork</ae-button>
+        </div>
+      </div>
+      <div v-if="isError" class="w-full p-4">
+        <div class="w-full flex justify-center p-4">
+          <span class="text-xl text-red">{{error}}</span>
+        </div>
+        <div class="w-full flex justify-center p-4">
+          <ae-button  type="normal" @click="reset">Ok</ae-button>
         </div>
       </div>
     </div>
@@ -31,7 +39,7 @@
 
 <script>
 
-  const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3
+  const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_ERROR = 3
 
   export default {
     name: 'Upload',
@@ -40,7 +48,8 @@
       return {
         fileCount: 0,
         currentStatus: 0,
-        uploadError: null
+        uploadError: null,
+        error: ''
       }
     },
     computed: {
@@ -50,19 +59,35 @@
       isSuccess () {
         return this.currentStatus === STATUS_SUCCESS
       },
-      uploadedImage() {
-        return this.$store.state.uploadedImage;
+      isError () {
+        return this.currentStatus === STATUS_ERROR
+      },
+      uploadedImage () {
+        return this.$store.state.uploadedImage
       }
     },
     methods: {
-      reset() {
-        this.currentStatus = STATUS_INITIAL;
+      reset () {
+        this.currentStatus = STATUS_INITIAL
       },
       filesChange (fileList) {
         // handle file changes
         const file = fileList[0]
-        this.$store.dispatch(`uploadImage`, file);
-        this.currentStatus = STATUS_SUCCESS;
+
+        if (file.size > 1000000) {
+          this.currentStatus = STATUS_ERROR
+          this.error = 'Maximum allowed file size is 1 MB'
+          return
+        }
+
+        if (!file.type.includes('image')) {
+          this.currentStatus = STATUS_ERROR
+          this.error = 'Please upload an image'
+          return
+        }
+
+        this.$store.dispatch(`uploadImage`, file)
+        this.currentStatus = STATUS_SUCCESS
       }
     }
   }
