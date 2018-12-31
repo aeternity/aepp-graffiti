0d8534bb967038a288122c89b01b7894d0034418
Philipp Piwo <github@piwo.space>
2018-12-31 12:51:09 +0100

    split server/logic; return correct svg content type

--- node-server/server.js
+++ node-server/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const canvas = require('./canvas.js');
-const ipfsWrapper = require('./ipfs.js');
 const fileUpload = require('express-fileupload');
 const app = express();
-const ImageTracer = require('./imagetracer.js');
-const {createCanvas, Image} = require('canvas');
-
+const logic = require('./logic');
 // TODO POST image --> svg
 
 app.use(fileUpload({
@@ -15,102 +12,13 @@ app.use(fileUpload({
 
 app.get('/canvas.png', canvas.shouldRender, canvas.send);
 
-app.get('/ipfs', (req, res) => {
-    ipfsWrapper.getFile('QmQjqVu5qsd4PPqJnTcLXmvznMw7X2UEjtLP9NKCtwWMx3').then((buffer) => {
+app.get('/ipfs', logic.ipfs);
 
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': buffer.length
-        });
-        res.end(buffer);
+/* upload form-data image key */
+app.post('/upload', logic.upload);
 
-    }).catch(e => {
-
-        res.send(e.message);
-
-    });
-});
-
-/*  upload POST endpoint */
-app.post('/upload', async (req, res) => {
-    if (Object.keys(req.files).length === 0) {
-        return res.status(422).json({
-            error: 'File needs to be provided.',
-        });
-    }
-
-    const file = req.files.image;
-
-    const mime = file.mimetype;
-    if (mime.split('/')[0] !== 'image') {
-        return res.status(422).json({
-            error: 'File needs to be an image.',
-        });
-    }
-
-    try {
-        const result = await ipfsWrapper.writeFile(file.data);
-        return res.json({
-            hash: result[0].hash,
-        });
-    } catch (e) {
-        return res.status(500).json({
-            error: e,
-        });
-    }
-});
-
-
-/*  upload POST endpoint */
-app.post('/transform', async (req, res) => {
-    if (Object.keys(req.files).length === 0) {
-        return res.status(422).json({
-            error: 'File needs to be provided.',
-        });
-    }
-
-    const file = req.files.image;
-
-    const mime = file.mimetype;
-    if (mime.split('/')[0] !== 'image') {
-        return res.status(422).json({
-            error: 'File needs to be an image.',
-        });
-    }
-
-    try {
-        file.type = file.mimetype;
-
-        const loadedImage = await new Promise((resolve, reject) => {
-            // Resolve source and img when loaded
-            const img = new Image();
-            img.onerror = () => reject(new Error('Couldn\'t load image'));
-            img.onload = () => resolve(img);
-            img.src = 'data:image/png;base64,' + file.data.toString('base64');
-        });
-
-        const fakeCanvas = createCanvas(canvas.width, canvas.height);
-        const ctx = fakeCanvas.getContext('2d');
-        ctx.drawImage(loadedImage, 0, 0);
-        const imageData = ctx.getImageData(0, 0, loadedImage.width, loadedImage.height);
-
-        console.log(loadedImage.width, loadedImage.height);
-
-        const myImageData = {width: loadedImage.width, height: loadedImage.height, data: imageData};
-
-        // tracing to SVG string
-        const options = {scale: 1}; // options object; option preset string can be used also
-
-        const svgstring = ImageTracer.imagedataToSVG(imageData, options);
-
-        res.send(svgstring);
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({
-            error: e,
-        });
-    }
-});
+/* upload POST endpoint */
+app.post('/transform', logic.transform);
 
 app.use((req, res) => {
     res.sendStatus(404);
