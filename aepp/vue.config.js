e192f4bcff1cfac125b9f735d5152107e678c670
Keno Dressel <git@keno.digital>
2019-01-02 17:07:32 +0100

    reworks deploy pipeline

---
+++ aepp/vue.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                // use this if you are installing the SDK as dependency
+                AE_SDK_MODULES: path.resolve(__dirname, 'node_modules/@aeternity/aepp-sdk/es/')
+                // use this, if you are running this app from inside the Aepp-SDK repo/folder
+                // AE_SDK_MODULES: '../../../../../es/'
+            }
+        }
+    }
+};
