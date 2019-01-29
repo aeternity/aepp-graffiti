c590422e900089a4d53cf66a17a4b5edf8b8aa56
keno dressel <aeternity@keno.digital>
2019-01-29 13:57:36 +0100

    updates visual for onboarding (#64)

--- aepp/src/components/OnboardingStep.vue
+++ aepp/src/components/OnboardingStep.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="p-4 flex flex-col justify-center items-center">
-      <slot></slot>
+    <div class="p-8 flex flex-col justify-center items-center">
+      <div class="w-full flex justify-center items-center ae-image-height">
+        <img :src="require(`@/assets/${image}`)" class="onboarding-logo" alt="onboarding step" />
+      </div>
+      <div>
+        <div class="mt-4 pl-2 pr-2">
+          <h2 class="text-center ">{{headline}}</h2>
+        </div>
+        <div class="text-center mt-2 pl-2 pr-2">
+          <span class="text-grey-darkest text-xl">{{text}}</span>
+        </div>
+      </div>
+
     </div>
 </template>
 
 <script>
   export default {
-    name: 'OnboardingStep'
+    name: 'OnboardingStep',
+    props: [ 'image', 'headline', 'text' ]
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .ae-image-height {
+    height: 50vh
+  }
 </style>
--- aepp/src/views/Onboarding.vue
+++ aepp/src/views/Onboarding.vue
@@ -1,74 +1,58 @@
 <template>
   <div>
-    <div class="absolute pin-t pin-r p-4" @click="quit()">
-      <ae-icon class="text-xl" name="close"></ae-icon>
-    </div>
-    <OnboardingStep v-if="view === 1" key="v1">
-      <div class="w-full flex justify-center items-center">
-        <img src="@/assets/0_DGP_lockup_black_1.svg" class="onboarding-logo" />
-      </div>
-      <div class="mt-4">
-        <ae-text align="center" weight="bold" class="text-center" fill="primary" face="sans-l">Welcome!</ae-text>
-      </div>
-      <div class="text-center w-2/3">
-        <ae-text align="center" class="text-center" face="sans-base" >Let drones spray your creative ideas on a wall in Mexico City.</ae-text>
-      </div>
+    <OnboardingStep
+      v-if="view === 1"
+      key="v1"
+      image="0_DGP_lockup_black_1.svg"
+      headline="Have a drone paint your art on a real wall in Mexico City"
+      text="Bid with AE to win drone time for your art"
+    >
     </OnboardingStep>
-    <OnboardingStep v-if="view === 2" key="v2">
-      <div class="w-full flex justify-center items-center">
-        <img src="@/assets/0_DGP_lockup_black_1.svg" class="onboarding-logo" />
-      </div>
-      <div class="mt-4">
-        <ae-text align="center" weight="bold" class="text-center" fill="primary" face="sans-l">How?</ae-text>
-      </div>
-      <div class="text-center w-2/3">
-        <ae-text align="center" class="text-center" face="sans-base" >Upload your own creation in this app. Refine the conversion settings and bid for drone time.</ae-text>
-      </div>
+    <OnboardingStep
+      v-if="view === 2"
+      key="v1"
+      image="0_DGP_lockup_black_1.svg"
+      headline="Upload an image and make an artwork out of it"
+      text="Your image will be vectorized, get creative with editing"
+    >
     </OnboardingStep>
-    <OnboardingStep v-if="view === 3" key="v2">
-      <div class="w-full flex justify-center items-center">
-        <img src="@/assets/0_DGP_lockup_black_1.svg" class="onboarding-logo" />
-      </div>
-      <div class="mt-4">
-        <ae-text align="center" weight="bold" class="text-center" fill="primary" face="sans-l">Bidding?</ae-text>
-      </div>
-      <div class="text-center w-2/3">
-        <ae-text align="center" class="text-center" face="sans-base" >Your artwork will take a certain amount of time for the drones to spray. The available time gets allocated to the highest bidders.</ae-text>
-      </div>
+    <OnboardingStep
+      v-if="view === 3"
+      key="v1"
+      image="0_DGP_lockup_black_1.svg"
+      headline="Bid enough AE to win the required drone time"
+      text="The aeternity smart contract sends the artworks of all successful bids to the drones"
+    >
     </OnboardingStep>
-    <OnboardingStep v-if="view === 4" key="v2">
-      <div class="w-full flex justify-center items-center">
-        <img src="@/assets/0_DGP_lockup_black_1.svg" class="onboarding-logo" />
-      </div>
-      <div class="mt-4">
-        <ae-text align="center" weight="bold" class="text-center" fill="primary" face="sans-l">Drones?</ae-text>
-      </div>
-      <div class="text-center w-2/3">
-        <ae-text align="center" class="text-center" face="sans-base" >If your bid is successful at the end of one auction slot our drones will spray your artwork on the wall in Mexico City.</ae-text>
-      </div>
+    <OnboardingStep
+      v-if="view === 4"
+      key="v1"
+      image="0_DGP_lockup_black_1.svg"
+      headline="Be part of the first smart contract art on this scale"
+      text="Checkout the wall and get creative"
+    >
     </OnboardingStep>
 
     <footer class="w-full fixed pin-b">
-      <div class="w-full flex flex-row justify-center">
-        <div v-for="step in MAX_VIEW" :class="step !== view ? 'ae-step-indicator' : 'ae-step-indicator-active'" :key="step">
+      <div class="w-full flex flex-row p-4">
+        <div @click="quit" class="w-1/3 p-2">
+          <ae-text face="uppercase-base" weight=700>SKIP</ae-text>
+        </div>
+        <div class="w-1/3 flex flex-row justify-center p-2">
+          <div v-for="step in MAX_VIEW" :class="step !== view ? 'ae-step-indicator' : 'ae-step-indicator-active'" :key="step">
+          </div>
+        </div>
+        <div class="w-1/3 flex justify-end p-2" @click="next"  v-if="nextText">
+          <ae-text face="uppercase-base" weight=700>{{nextText}}</ae-text>
         </div>
       </div>
-      <div class="w-full flex flex-row justify-between p-4">
-        <ae-button v-if="backText" fill="neutral" face="round" @click="back">
-          {{backText}}
-        </ae-button>
-
-        <ae-button v-if="nextText" fill="primary" class="ml-auto" face="round" @click="next">
-          {{nextText}}
-        </ae-button>
-      </div>
     </footer>
-
   </div>
 </template>
 
 <script>
   import OnboardingStep from '@/components/OnboardingStep'
+  import { AeText } from '@aeternity/aepp-components'
 
   export default {
     name: 'Onboarding',
@@ -80,17 +64,13 @@
       }
     },
     components: {
-      OnboardingStep
+      OnboardingStep,
+      AeText
     },
     computed: {
       nextText() {
-        if(this.view === 4) return 'Get Started'
-        else return 'Next'
-      },
-
-      backText() {
-        if(this.view === 1) return null
-        else return 'Back'
+        if(this.view === 4) return 'START'
+        else return 'NEXT'
       }
     },
     methods: {
@@ -102,11 +82,8 @@
           return this.$router.push('contribute');
         }
         this.view += 1
-      },
-      back() {
-        if(this.view === this.MIN_VIEW) return this.$router.push('home');
-        this.view -= 1
       }
+
     },
     mounted() {
       this.$store.dispatch('setFirstTimeOpenedFalse')
@@ -121,12 +98,12 @@
   }
 
   .ae-step-indicator {
-    background: #555;
+    background: #bbb;
     border-radius: 10px;
     height: 10px;
     width: 10px;
-    margin-left: 2px;
-    margin-right: 2px;
+    margin-left: 3px;
+    margin-right: 3px;
   }
 
   .ae-step-indicator-active {
