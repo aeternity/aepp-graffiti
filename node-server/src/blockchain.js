c244c6530702eb3a01b031a7d25eab7295f69730
Philipp Piwo <github@piwo.space>
2019-02-07 11:19:37 -0600

    adjust teaser mainnet deploy

--- node-server/src/blockchain.js
+++ node-server/src/blockchain.js
@@ -5,9 +5,10 @@ const blockchain = {};
 
 // can eventually be called by name in the future
 const contractAddress = 'ct_Lx1ppN3iEMyjEPLmJpda7iJwVQxhokmD8bUic9g1Z63tdGuSc';
-const teaserContractAddress = 'ct_Fk3wWdC7t6Fv5eypLWThND19CGn1cBuScnGBZx6r6tG9q1DbU';
+const teaserContractAddress = 'ct_2NHu5ra1puDr2RmuwJbfM3YNycsdq6B2P2UmVYZhKmr8mywmcP';
 
 let client = null;
+let mainnetClient = null;
 
 const aeternityUrl = process.env.AETERNITY_URL || 'http://localhost:3013';
 const aeternityInternalUrl = process.env.AETERNITY_URL || 'http://localhost:3113';
@@ -16,6 +17,11 @@ blockchain.init = async () => {
     client = await EpochChain.compose(EpochContract)({
         url: aeternityUrl,
         internalUrl: aeternityInternalUrl,
+    }).catch(console.error);
+
+    mainnetClient = await EpochChain.compose(EpochContract)({
+        url: "https://sdk-mainnet.aepps.com",
+        internalUrl:  "https://sdk-mainnet.aepps.com",
     }).catch(console.error);
 
     console.log('initialized aeternity sdk');
@@ -37,19 +43,19 @@ blockchain.auctionSlots = async () => {
 };
 
 blockchain.teaserArtworks = async () => {
-    if (!client) await blockchain.init();
+    if (!mainnetClient) await blockchain.init();
 
-    const called = await client.contractEpochCall(teaserContractAddress, 'sophia-address', 'all_artworks', '()').catch(console.error);
-    const decoded = await client.contractEpochDecodeData(TeaserUtil.artworkListType, called.out).catch(console.error);
+    const called = await mainnetClient.contractEpochCall(teaserContractAddress, 'sophia-address', 'all_artworks', '()').catch(console.error);
+    const decoded = await mainnetClient.contractEpochDecodeData(TeaserUtil.artworkListType, called.out).catch(console.error);
 
     return TeaserUtil.artworkListToObject(decoded);
 };
 
 blockchain.teaserGeolocation = async () => {
-    if (!client) await blockchain.init();
+    if (!mainnetClient) await blockchain.init();
 
-    const called = await client.contractEpochCall(teaserContractAddress, 'sophia-address', 'get_geolocation', '()').catch(console.error);
-    const decoded = await client.contractEpochDecodeData(TeaserUtil.geolocationType, called.out).catch(console.error);
+    const called = await mainnetClient.contractEpochCall(teaserContractAddress, 'sophia-address', 'get_geolocation', '()').catch(console.error);
+    const decoded = await mainnetClient.contractEpochDecodeData(TeaserUtil.geolocationType, called.out).catch(console.error);
 
     return TeaserUtil.geolocationToObject(decoded);
 };
--- smart-contract/deployment/teaserDeploy.js
+++ smart-contract/deployment/teaserDeploy.js
@@ -4,29 +4,35 @@ const Utils = require('../deployment/teaserUtils');
 const keypair = require('../keypair.json');
 
 const config = {
-    host: "https://sdk-testnet.aepps.com/",
-    internalHost: "https://sdk-testnet.aepps.com/"
+    host: "https://sdk-mainnet.aepps.com/",
+    internalHost: "https://sdk-mainnet.aepps.com/"
 };
 
 const init = async () => {
-    const {contract, client} = await deploy();
-    await addArtwork(contract, 1, 200, "QmUG21B7wEfCCABvcZpWKF31Aqc8H2fdGBZ4VSAP6vGvQd");
-    await addArtwork(contract, 2, 200, "QmUG21B7wEfCCABvcZpWKF31Aqc8H2fdGBZ4VSAP6vGvQd");
-};
-
-const deploy = async () => {
     const client = await Ae({
         url: config.host,
         internalUrl: config.internalHost,
         keypair: keypair,
         nativeMode: true,
-        networkId: 'ae_uat'
+        networkId: 'ae_mainnet'
     });
+
+    const contract = await deploy(client);
+    const contractAddress = contract.address;
+    //const contractAddress = "ct_2NHu5ra1puDr2RmuwJbfM3YNycsdq6B2P2UmVYZhKmr8mywmcP";
+
+    //await addArtwork(contractAddress, client, 1, 200, "QmUG21B7wEfCCABvcZpWKF31Aqc8H2fdGBZ4VSAP6vGvQd");
+    //await addArtwork(contractAddress, client, 2, 200, "QmUG21B7wEfCCABvcZpWKF31Aqc8H2fdGBZ4VSAP6vGvQd");
+};
+
+const deploy = async (client) => {
+
 
     let contractSource = await forgeutils.readFile('./contracts/DroneGraffitiTeaser.aes', "utf-8");
     const compiledContract = await client.contractCompile(contractSource, {gas: config.gas}).catch(console.error);
 
     const deployPromise = compiledContract.deploy({
+        //initState: '("19.421802,-99.158284")',
         initState: '("0.000000,-0.000000")',
         options: {
             ttl: config.ttl,
@@ -42,10 +48,13 @@ const deploy = async () => {
     return {contract, client};
 };
 
-const addArtwork = async (contract, artworkId, dronetime, artworkReference) => {
-    await contract.call('add_artwork', {args: `(${artworkId}, ${dronetime}, "${artworkReference}")`, options: {amount: 0}});
-    const staticCall = await contract.callStatic('all_artworks', {args: '()'});
-    const decoded = await staticCall.decode(Utils.artworkListType);
+const addArtwork = async (contractAddress, client, artworkId, dronetime, artworkReference) => {
+    await client.contractCall(contractAddress, 'sophia-address', contractAddress, 'add_artwork', {
+        args: `(${artworkId}, ${dronetime}, "${artworkReference}")`,
+        options: {amount: 0}
+    });
+    const staticCall = await client.contractCallStatic(contractAddress, 'sophia-address', 'all_artworks', {args: '()'});
+    const decoded = await client.contractDecodeData(Utils.artworkListType, staticCall.result);
 
     const artworks = Utils.artworkListToObject(decoded);
 
