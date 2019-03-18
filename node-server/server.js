cf05e71090716e98c8f7a4e96eeb7e5b01d309c8
keno dressel <aeternity@keno.digital>
2019-03-18 16:15:28 +0100

    adds error handler to external requests

--- node-server/server.js
+++ node-server/server.js
@@ -8,6 +8,18 @@ const bodyParser = require('body-parser');
 const storage = require('./src/storage');
 const health = require('./src/health');
 
+// HELPER FUNCTIONS
+
+const errorHandler = (asyncFunction) => {
+    return (req, res, next) => {
+        try {
+            asyncFunction(req, res, next);
+        } catch (e) {
+            next(e);
+        }
+    }
+};
+
 app.use(bodyParser.json());
 app.use(cors({
     origin: '*',
@@ -22,37 +34,51 @@ app.use(fileUpload({
 
 app.use('/rendered', express.static(__dirname + '/data/rendered'));
 
-app.get('/ipfs/:hash.svg', logic.ipfs);
+app.get('/ipfs/:hash.svg', errorHandler(logic.ipfs));
 
 // upload form-data image key
-app.post('/upload', logic.upload);
+app.post('/upload', errorHandler(logic.upload));
 
 // get slot export zip
-app.get('/slots/:id', logic.getSlots);
+app.get('/slots/:id', errorHandler(logic.getSlots));
 
 // get data for teaser event
-app.get('/teaser.json', logic.teaserJson);
+app.get('/teaser.json', errorHandler(logic.teaserJson));
 
-app.get('/bid/:id', logic.getSingleBid);
+app.get('/bid/:id', errorHandler(logic.getSingleBid));
 
-app.get('/health/ipfsNode', health.ipfsNode);
-app.get('/health/blockchainNode', health.blockchainNode);
-app.get('/health/teaserFiles', health.teaserIPFSFiles);
-app.get('/health/teaserContract', health.teaserSmartContract);
-app.get('/health/testFiles', health.normalIPFSFiles);
-app.get('/health/testContract', health.normalSmartContract);
+app.get('/health/ipfsNode', errorHandler(health.ipfsNode));
+app.get('/health/blockchainNode', errorHandler(health.blockchainNode));
+app.get('/health/teaserFiles', errorHandler(health.teaserIPFSFiles));
+app.get('/health/teaserContract', errorHandler(health.teaserSmartContract));
+app.get('/health/testFiles', errorHandler(health.normalIPFSFiles));
+app.get('/health/testContract', errorHandler(health.normalSmartContract));
+
+app.use((err, req, res) => {
+    console.error(err.stack);
+    if(res.headersSent) return;
+    res.status(500).send(e.message);
+});
 
 app.use((req, res) => {
     res.sendStatus(404);
 });
 
 try {
-    canvas.init()
+    canvas.init();
 } catch (e) {
     console.warn("First canvas.init call failed, trying again in 30 seconds");
-    setTimeout(() => canvas.init(), 30000);
+    try {
+        setTimeout(() => canvas.init(), 30000);
+    } catch (e) {
+        console.error("Could not connect to blockchain client.");
+    }
 }
 
+try {
+    storage.init();
+} catch (e) {
+    console.error("Could not initialize to storage.");
+}
 
-storage.init();
 app.listen(3000);
