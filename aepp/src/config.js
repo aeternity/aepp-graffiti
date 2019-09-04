d3482538e5bd45dfc9723c8ad8770798c749628a
Philipp Piwo <github@piwo.space>
2019-09-04 17:22:10 +0200

    updated mainnet deployment

--- aepp/src/config.js
+++ aepp/src/config.js
@@ -32,7 +32,7 @@ export default {
   },
   blockchainSettings: {
     ae_uat: 'ct_eU2JXB8pTDMMofPdtNW6XwMmVkSvp3iKXcoeLshreHEtBeApa', // testnet //ct_2SugvqB52guEV6fSTkuGjipzPMjeEf5urYQYnPe91cdkMmjJGy
-    ae_mainnet: 'ct_2GWJrLVVqxoV9hx7smEThtQ9TJ6fcD1SRh3LKJ74HuxMD6cgqn' // 'ct_2P2vEqq3WQz6kzKLJFoqBbm46EMot64WvpP1xpvvANApLWcwnt';
+    ae_mainnet: 'ct_Hk2JsNeWGEYQEHHQCfcBeGrwbhtYSwFTPdDhW2SvjFYVojyhW' // 'ct_2P2vEqq3WQz6kzKLJFoqBbm46EMot64WvpP1xpvvANApLWcwnt';
   },
   apiUrl: API_URL
 }
