e447eb82b941235f46416905dff2663248e7a101
keno dressel <aeternity@keno.digital>
2019-09-25 12:54:12 +0200

    updates sdk and dependencies to 4.7.0

--- aepp/babel.config.js
+++ aepp/babel.config.js
@@ -4,13 +4,16 @@ module.exports = {
     '@babel/preset-env',
     {
       debug: false,
-      useBuiltIns: 'usage'
+      useBuiltIns: 'usage',
+      corejs: 3
     }
   ]],
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-modules-commonjs',
-    '@babel/plugin-transform-runtime',
+     ['@babel/plugin-transform-runtime', {
+        corejs: 3,
+      }],
     '@babel/plugin-transform-block-scoping'
   ]
 }
