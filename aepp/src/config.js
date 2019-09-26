099397cffa62d60d9a95ae6e17d0a59610e4c85e
Philipp Piwo <github@piwo.space>
2019-09-26 17:16:20 +0200

    updated backend url

--- aepp/src/config.js
+++ aepp/src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://backend.graffiti.aeternity.com'
+const API_URL = 'https://backend.graffiti.aeternity.art'
 // const API_URL = 'http://localhost:3000';
 // const API_URL = 'http://192.168.0.157:3000';
 
