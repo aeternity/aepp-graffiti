ea342ac2b41257f63d59ee67096b3e41e6375ce0
keno dressel <aeternity@keno.digital>
2019-09-26 18:31:16 +0200

    adds purge css to reduce bundle size

--- aepp/postcss.config.js
+++ aepp/postcss.config.js
@@ -1,9 +1,21 @@
+const path = require('path')
+
 module.exports = {
   parser: 'postcss',
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        path.join(__dirname, './src/index.html'),
+        path.join(__dirname, './**/*.vue'),
+        path.join(__dirname, './src/**/*.js'),
+        path.join(__dirname, './node_modules/@aeternity/aepp-components/**/*.vue'),
+        path.join(__dirname, './node_modules/@aeternity/aepp-components/**/*.js')
+      ],
+      whitelistPatterns: [/^ae/]
+    }),
     require('cssnano')({
       'preset': [
         'default',
