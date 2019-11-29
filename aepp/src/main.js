1bccaa84c668d716d517b2b6b600e68eda91f96f
keno dressel <aeternity@keno.digital>
2019-11-29 13:57:50 +0100

    cleans up main.js

--- aepp/src/main.js
+++ aepp/src/main.js
@@ -1,10 +1,7 @@
-// import socketio from 'socket.io'
 import './main.css'
-/**
- * This will only include font files
- */
+
 import '@aeternity/aepp-components/dist/aepp.fonts.css'
-import '@aeternity/aepp-components/dist/aepp.components.css'
+import '@aeternity/aepp-components/dist/ae-icon/ae-icon.css'
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -12,49 +9,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-/**
- * Documentation of the new components are here.
- *
- * http://aeternity.com/aepp-components/
- */
-
-//import Components from '@aeternity/aepp-components'
-//Vue.use(Components)
-
-/**
- * Use this if you need a general reset.
- *
- * Includes global style changes like:
- *
- * - all needed fonts
- * - normalize.css
- *
- * html, body {} styles (like font, font-size)
- * p {}
- * h1, h2, h3, h4, h5, h6 {}
- */
-// import '@aeternity/aepp-components/dist/aepp.global.css'
-
-/**
- * These are all components styles. They're scoped
- * so they won't collide with other styles.
- */
-
 Vue.use(VueRouter)
-
 Vue.config.productionTip = false
-
-console.info('about to render Vue App')
-
-/**
- * I would suggest exporting this Vue instance.
- *
- * In the future it might turn useful if you want to reference it
- * from some other packages that have no relation with VueJS
- *
- * Also looks nice.
- */
-
 
 export default new Vue({
   router: router,
