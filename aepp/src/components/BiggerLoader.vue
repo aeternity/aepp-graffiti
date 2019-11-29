00748f8e918484191bef7c4b7f59a655210e8afd
keno dressel <aeternity@keno.digital>
2019-11-29 14:17:06 +0100

    replaces loader with aeternity logo

--- aepp/src/components/BiggerLoader.vue
+++ aepp/src/components/BiggerLoader.vue
@@ -1,34 +1,15 @@
 <template>
   <div class="mt-8 relative flex justify-center">
-    <ae-loader class="ae-loader-scaling"></ae-loader>
-    <div class="absolute ae-loader-progress w-full">{{progress}}</div>
+    <img src="../assets/loading_logo.svg" alt="loading logo" />
   </div>
 </template>
 
 <script>
-
-  import { AeLoader } from '@aeternity/aepp-components/src/components'
-
   export default {
     name: "BiggerLoader",
-    components: {AeLoader},
-    props: ['progress']
   }
 </script>
 
 <style scoped>
-  .ae-loader-progress {
-    font-size: 1em;
-    top: 2em;
-    left: 0;
-    text-align: center;
-  }
-
-  .ae-loader-scaling {
-    width: 5em;
-    height: 5em;
-    border-radius: 5em;
-    border-width: 0.4em;
-  }
 
 </style>
